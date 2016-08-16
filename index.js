ada3fb4abeb941a14ba434f29cfe287393cfc0e5
Seth Tippetts <sethtippetts@gmail.com>
2016-08-16 14:33:52 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const { diffWordsWithSpace } = require('jsdiff')
+
+const { formatLists, parseLists } = require('./lib/list')
+const { wrapChange } = require('./lib/util')
+
+module.exports = function richDiff (str1, str2) {
+  const { text: text1, list: list1 } = parseLists(str1)
+  const { text: text2, list: list2 } = parseLists(str2)
+
+  return diffWordsWithSpace(text1, text2)
+    .map(({ value, added, removed }) => {
+      const hasList = value.includes('{{{ LIST }}}')
+      const hasChange = added ^ removed ? !!added : null
+      if (hasList) {
+        return formatLists(value, list1, list2, hasChange)
+      }
+      return wrapChange(value, hasChange)
+    })
+    .join('')
+}
+
