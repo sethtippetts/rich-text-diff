b3bb73fd0095facced703982cc0b16a5bf51b90e
Seth Tippetts <sethtippetts@gmail.com>
2016-08-16 17:05:59 -0600

    Revert "Remove all `const` and `let` declarations"
    This reverts commit c1c69c3c45e85162c7456d81b8d031efa2315001.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict'
 
-var diffWordsWithSpace = require('diff').diffWordsWithSpace
+const diffWordsWithSpace = require('diff').diffWordsWithSpace
 
-var formatLists = require('./lib/list/format')
-var parseLists = require('./lib/list/parse')
-var wrapChange = require('./lib/util').wrapChange
+const formatLists = require('./lib/list/format')
+const parseLists = require('./lib/list/parse')
+const wrapChange = require('./lib/util').wrapChange
 
 module.exports = function richDiff (str1, str2) {
-  var parsed1 = parseLists(str1)
-  var parsed2 = parseLists(str2)
+  const parsed1 = parseLists(str1)
+  const parsed2 = parseLists(str2)
 
   return diffWordsWithSpace(parsed1.text, parsed1.text)
     .map(function (change) {
-      var hasList = change.value.includes('{{{ LIST }}}')
-      var hasChange = change.added ^ change.removed ? !!change.added : null
+      const hasList = change.value.includes('{{{ LIST }}}')
+      const hasChange = change.added ^ change.removed ? !!change.added : null
       if (hasList) {
         return formatLists(change.value, parsed1.list, parsed2.list, hasChange)
       }
