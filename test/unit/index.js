d91c4014e04172bb8043b6bddd275b1eaf34d52b
James Black <james@james-black.com>
2016-09-16 15:50:15 -0600

    add more tests, one fails this time

--- test/unit/index.js
+++ test/unit/index.js
@@ -54,22 +54,40 @@ I really hope, that it does. If it <del>doesn\'t</del><ins>does,</ins> I <del>mi
 
   describe('Edge Cases', () => {
     it('should handle a paragraph becoming a list', () => {
-      let left = `
+      let left, right, target, diff
+      left = `
 1. This is a paragraph that will become a list. 2. Don't you know? 3. I hope it works!
       `
-      let right = `
+      right = `
 1. This is a paragraph that will become a list.
 2. Don't you know?
 3. I hope it works!
       `
-      let target = `
-0. <del>This is a paragraph that will become a list. 2. Don't you know? 3. I hope it works!</del>
-1. <ins>This is a paragraph that will become a list.</ins>
-2. <ins>Don't you know?</ins>
-3. <ins>I hope it works!</ins>
+      target = `
+<del>1. This is a paragraph that will become a list. 2. Don't you know? 3. I hope it works!</del>
+<ins>1. This is a paragraph that will become a list.</ins>
+<ins>2. Don't you know?</ins>
+<ins>3. I hope it works!</ins>
       `
-      let diff = richDiff(left, right)
-      assert.equal(diff, target)
+      diff = richDiff(left, right)
+      assert.equal(diff, target, 'fails when paragraph starts with a list delimiter')
+      left = `
+This is a paragraph - item 1 - item 2
+      `
+      right = `
+This is a paragraph
+- item 1
+- item 2
+      `
+      target = `
+This is a paragraph<del> - item 1 - item 2</del>
+- <ins>item 1</ins>
+- <ins>item 2</ins>
+      `
+      diff = richDiff(left, right)
+      console.log(diff)
+      assert.equal(diff, target, 'fails when paragraph starts with normal text')
+
     })
   })
 })
